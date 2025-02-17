14a13eb3a2654308cc5b1495b4c3435c8f88ca35
luffyerrot <luffyerrot20@gmail.com>
2025-02-17 21:40:46 +0100

    Ajout de la conteneurisation

--- src/layouts/test/default.spec.ts
+++ src/layouts/test/default.spec.ts
@@ -1,6 +1,6 @@
 import { factory, renderCorrectly } from "@/utils/test";
 import { describe } from "vitest";
-import Default from "../Default.vue";
+import Default from "../default.vue";
 
 describe('Page principale', () => {
     const wrapper = factory(Default, true);
